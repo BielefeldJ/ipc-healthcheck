d0ade5327d62f91e0d127079f1eb4090351c395b
Bielefeld <thejens90@gmail.com>
2022-01-31 12:39:58 +0100

    added notify to example scripts

--- example-client.js
+++ example-client.js
@@ -1,16 +1,36 @@
 //importing the client from ipc-healthcheck
 const HealthcheckClient = require('ipc-healthcheck/healthcheck-client');
 
+
 //this is the name used to register at the server. The server will print out this name, then this service crashes
 const serviceID = 'ExampleService';
 //Turns debbug messages on or off. If set to false, the HealthcheckClient will print out log and status messages to console.
-const silent = false;
+const silent = true;
 
 //Create the client instance with the settings set above
 const healthcheckclient = new HealthcheckClient(serviceID,silent);
 
 //start listening to healthcheck calls
 healthcheckclient.startListening();
+
+//Example: We run into an error of some sort and want to be informed about this.
+//So in case we catch an error, we notify the server about this. 
+//this will trigger after 3 seconds. 
+setTimeout( () => {
+	try{
+		//do something that might fail
+		if(Math.floor(Math.random()*2) === 0)
+		{
+			//there is a 50:50 that this will be executed and throw an error.
+			letsthrowanerror.trigger;
+		}
+	}
+	catch(e){
+		//send the error message to the server.
+		healthcheckclient.notifyAboutError(e.stack);
+	}
+}, 3000);
+
 
 //setTimeout is not required. I use it only for demonstration. This example client will shutdown after 5 secconds.
 //You can simulate a crash if you interrupt this script with CTRL+C or simply by killing the process.
--- example-server.js
+++ example-server.js
@@ -2,19 +2,28 @@
 //importing the HealthcheckServer.
 const HealthcheckServer = require('ipc-healthcheck/healthcheck-server');
 
+
 //Time the server waits before checking for a response from the client. Time in ms.
 const respondTime = 100;
 //Time the server waits between checks. Time in ms. The intervalTime should always be greater than respondTime
 const intervalTime = 1000
 //Turns debbug messages on or off. If set to false, the HealthcheckServer will print out log and status messages to console.
-const silent = false;
+const silent = true;
 
 //Create the HealthcheckServer with the given settings.
 const healthcheckserver = new HealthcheckServer(respondTime,intervalTime ,silent);
 
-//this event will trigger, as sool as a service did not respond 3 times in a row. 
+//this event will trigger, as soon as a service did not respond 3 times in a row. 
 healthcheckserver.on('serviceCrashed', (name) => {
-	console.log("Service crashed: " + name);
+	console.log('Service crashed: ' + name);
+});
+
+//this event will trigger, as soon as a service calls the notifyAboutError method.
+//This way, the service can send a message to the server.
+//An example would be, if the service in question throws an error and you want to be informed about it.
+//in this case you can call the notifyAboutError method client side and this event will trigger here
+healthcheckserver.on('serviceError', (errmeg,service) => {
+	console.log(`Service ${service.name} with id ${service.id} send the following error: ${errmeg}`);
 });
 
 //Starts the HealthcheckServer
